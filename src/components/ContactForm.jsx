22bd6b27f9e65f91278fbc9e3f895adad1ff85e0
SamGu-NRX <sgu07966@gmail.com>
2024-06-06 10:56:52 -0700

    Improve contact form formatting

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -47,113 +47,123 @@ const ContactForm = () => {
 
     const handleConfirmClick = () => {
         window.location.href = 'https://forms.gle/RaW38zynf2p515Ua8';
+        // send an email to admin@luminai.com
     };
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4">
         <div>
             <label className="block mb-1">
-                Name:
+                Name
             </label>
-            <input type="text" name="name" className="w-full p-2 border border-gray-300 rounded" />
+            <input type="text" name="name" className="w-full p-2 border border-gray-300 rounded" placeholder="Enter your name" required/>
         </div>
+    
         <div>
-          <label className="block text-gray-700">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md p-2 border"
-          />
-          {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-        </div>
-
-        "// Select where user heard us from"
-        <div>
-          <label className="block text-gray-700">How did you hear about us?</label>
-          <select
-            name="heardFrom"
-            value={formData.heardFrom}
-            onChange={handleChange}
-            className="mt-1 block w-full border p-2 border-gray-300 rounded-md"
-          >
-            <option value="">Select an option</option>
-            <option value="internet search">Internet Search</option>
-            <option value="reddit">Reddit</option>
-            <option value="discord">Discord</option>
-            <option value="school announcement">School Announcement</option>
-            <option value="other">Other (Please specify)</option>
-          </select>
-          {formData.heardFrom === 'other' && (
+            <label className="block text-gray-700">
+            Email
+            </label>
             <input
-              type="text"
-              name="additionalInfo"
-              placeholder="Please specify"
-              value={formData.additionalInfo}
-              onChange={handleChange}
-              className="mt-1 block w-full border-gray-300 rounded-md"
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 block w-full border-gray-300 rounded-md p-2 border"
+                placeholder="Enter your full email" required
             />
-          )}
-          {errors.heardFrom && <p className="text-red-500 text-sm">{errors.heardFrom}</p>}
+            {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
         </div>
 
-        "// Select the type of question for inquiry, and change the message flex box if necessary"
 
         <div>
-          <label className="block text-gray-700">Type of question</label>
-          <select
-            name="questionType"
-            value={formData.questionType}
-            onChange={handleChange}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-          >
-            <option value="">Select a type</option>
-            <option value="improve">Help us improve!</option>
-            <option value="course/camp">Questions about course/camp</option>
-            <option value="collaboration/business">Contact for collaboration/business reasons</option>
-          </select>
-          {formData.questionType === 'course/camp' && (
+            <label className="block text-gray-700">How did you hear about us?</label>
             <select
-              name="course"
-              value={formData.course}
-              onChange={handleChange}
-              className="mt-1 block w-full border-gray-300 rounded-md"
+                name="heardFrom"
+                value={formData.heardFrom}
+                onChange={handleChange}
+                className="mt-1 block w-full border p-2 border-gray-500 rounded-md"
+                required
             >
-              <option value="">Select a course</option>
-              <option value="AI innovate scholars">AI Innovate Scholars</option>
+                <option value="">Select an option</option>
+                <option value="internet search">Internet Search</option>
+                <option value="reddit">Reddit</option>
+                <option value="discord">Discord</option>
+                <option value="school announcement">School Announcement</option>
+                <option value="other">Other (Please Specify)</option>
             </select>
-          )}
-          {errors.questionType && <p className="text-red-500 text-sm">{errors.questionType}</p>}
+            {formData.heardFrom === 'other' && (
+                <input
+                type="text"
+                name="additionalInfo"
+                placeholder="Please specify"
+                value={formData.additionalInfo}
+                onChange={handleChange}
+                className="mt-1 block w-full border-gray-300 rounded-md border p-2"
+                required
+                />
+            )}
+            {errors.heardFrom && <p className="text-red-500 text-sm">{errors.heardFrom}</p>}
         </div>
-        
+
+    "// Select the type of question for inquiry, and change the message flex box if necessary"
+
         <div>
-        
-        <label className="block mb-1">Message:</label>
-        <textarea name="message" className="w-full p-2 border border-gray-300 rounded"></textarea>
-      
+            <label className="block text-gray-700">Type of question</label>
+            <select
+                name="questionType"
+                value={formData.questionType}
+                onChange={handleChange}
+                className="mt-1 block w-full p-2 border border-gray-500 rounded-md"
+                required
+            >
+                <option value="">Select a question</option>
+                <option value="improve">Help us improve!</option>
+                <option value="course/camp">Questions about course/camp</option>
+                <option value="collaboration/business">Contact for collaboration/business reasons</option>
+            </select>
         </div>
-        
-        
+
+
         <div>
-          <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-            Submit
-          </button>
+        {formData.questionType === 'course/camp' && (
+            <div>
+                <label className="block text-gray-700">Course Name</label>
+                <select name="course" value={formData.course} onChange={handleChange}
+                    className="mt-1 block w-full border-gray-300 rounded-md border p-2">
+                    <option value="">Select a course</option>
+                    <option value="AI innovate scholars">AI Innovate Scholars</option>
+                </select>
+            </div>
+        )}
+        {errors.questionType && <p className="text-red-500 text-sm">{errors.questionType}</p>}
         </div>
-      </form>
-      <Modal
+        
+        <div>    
+            <label className="block mb-1">
+                Message
+            </label>
+            <textarea name="message" className="w-full p-2 border border-gray-300 rounded" placeholder="Enter your message" required></textarea>
+        </div>
+    
+
+        <div>
+            <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
+                Submit
+            </button>
+        </div>
+
+    </form>
+    <Modal
         isOpen={isOpen}
         onClose={closeModal}
-        onConfirm={handleConfirmClick}
-      >
-        <p className="mb-4">We have received your message. You will hear back from our team soon.</p>
-      </Modal>
+        onConfirm={handleConfirmClick}>
+        <p className="mb-4">
+            We have received your message. You will hear back from our team soon.
+        </p>
+    </Modal>
     </div>
   );
 };
 
 export default ContactForm;
-
-
-      
