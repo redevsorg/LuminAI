d1c860c20e273886620f1f58e3c9466d47696ad1
cooperzilla <cooperye09@gmail.com>
2024-06-10 21:50:11 -0500

    react 19 and compiler

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,10 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'eslint-plugin-react-compiler'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'react-compiler/react-compiler': "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
